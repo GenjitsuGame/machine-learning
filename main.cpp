5452ce925ef996cbb90b2f186920b9be8d0de487
GenjitsuGame <pascal.luttgens@opendatasoft.com>
2017-04-13 22:18:55 +0200

    format code, update linear regression, add 2D get

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include <cassert>
 
 
-
 int sign(float input) {
     if (input >= 0) return 1;
     else return -1;
@@ -12,15 +11,28 @@ void linear_regression(float *model, float *input) {
     assert(model && *model);
     assert(input && *input);
 
+    const int modelSize = (sizeof(model) / sizeof(*model));
+    const int inputSize = (sizeof(input) / sizeof(*input));
+    int *outputs = new int[modelSize * inputSize];
 
+    for (int i = 0; i < inputSize; ++i) {
+        for (int j = 0; j < modelSize; ++j) {
+
+        }
+    }
 }
 
 int get_random_example(float *inputs) {
     assert(inputs && *inputs);
 
-    srand ((unsigned int) time(NULL));
-    const int size = (sizeof(inputs)/sizeof(*inputs));
+    srand((unsigned int) time(NULL));
+    const int size = (sizeof(inputs) / sizeof(*inputs));
     return rand() % size;
+}
+
+template<typename T>
+T two_dim_get(const T *array, int width, int i, int j) {
+    return array[width * j + i];
 }
 
 int main() {
