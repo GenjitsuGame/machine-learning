de07b3850bfa07e57c1b728f9497d91908b22dbf
GenjitsuGame <pascal.luttgens@opendatasoft.com>
2017-04-13 22:03:54 +0200

    add sign, get_random_example, linear_regression body

--- main.cpp
+++ main.cpp
@@ -1,4 +1,25 @@
 #include <iostream>
+#include <cassert>
+
+
+
+int sign(float input) {
+    if (input >= 0) return 1;
+    else return -1;
+}
+
+void linear_regression(float *model, float *input, int epochs) {
+    assert(epochs > 0 && input && model);
+
+}
+
+int get_random_example(float *inputs) {
+    assert(inputs && *inputs);
+
+    srand ((unsigned int) time(NULL));
+    const int size = (sizeof(inputs)/sizeof(*inputs));
+    return rand() % size;
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
